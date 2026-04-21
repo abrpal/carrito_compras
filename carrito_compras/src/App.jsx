9c176f4c2849a5933d28987422980b630c052d03
Abraham <a.palomino.gz@gmail.com>
2026-04-20 18:04:04 -0600

    Added cart manipulation logic and persistance with localStorage

--- carrito_compras/src/App.jsx
+++ carrito_compras/src/App.jsx
@@ -6,8 +6,19 @@ import { db } from './data/db.js'
 
 function App() {
 
+  const intialCart = () =>{
+    const localStorageCart = localStorage.getItem("cart");
+    return localStorageCart ? JSON.parse(localStorageCart) : [];
+  }
+
   const [data, setData] = useState(db);
-  const [cart, setCart] = useState(new Array().fill(0));
+  const [cart, setCart] = useState(intialCart);
+
+  const MAX_ITEMS = 5;
+
+  useEffect(() => {
+    localStorage.setItem("cart", JSON.stringify(cart));
+  }, [cart]);
 
   function addToCart(item){
     const itemIndex = cart.findIndex(guitar => guitar.id === item.id);
@@ -16,16 +27,46 @@ function App() {
       item.quantity = 1;
       setCart([...cart, item]);
     }else{
+      if(cart[itemIndex].quantity >= MAX_ITEMS) return;
       let updatedCart = [...cart];
       updatedCart[itemIndex].quantity++;
       setCart(updatedCart);
     }
-    
+  }
+
+  function removeFromCart(id){
+    setCart(prevCart => prevCart.filter((item) => item.id !==id));
+  }
+
+  function increaseQuantity(id){
+    let updatedCart = cart.map(item => {
+      if(item.id === id && item.quantity < MAX_ITEMS) item.quantity++;
+      return item;
+    })
+    setCart(updatedCart);
+  }
+
+  function decreaseQuantity(id){
+    let updatedCart = cart.map(item => {
+      if(item.id === id && item.quantity > 1) item.quantity--;
+      return item;
+    })
+    setCart(updatedCart);
+  }
+
+  function clearCart(){
+    setCart([])
   }
 
   return (
     <>
-    <Header />
+    <Header 
+      cart = {cart}
+      removeFromCart={removeFromCart}
+      increaseQuantity={increaseQuantity}
+      decreaseQuantity={decreaseQuantity}
+      clearCart={clearCart}
+    />
 
     <main className="container-xl mt-5">
         <h2 className="text-center">Nuestra Colección</h2>
