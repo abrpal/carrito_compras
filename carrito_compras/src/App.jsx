748bc4a1be0f4af5de4dabd3c70d6548f8a53df6
Abraham <a.palomino.gz@gmail.com>
2026-04-21 19:24:33 -0600

    Final adjustments for deployment

--- carrito_compras/src/App.jsx
+++ carrito_compras/src/App.jsx
@@ -11,7 +11,7 @@ function App() {
     return localStorageCart ? JSON.parse(localStorageCart) : [];
   }
 
-  const [data, setData] = useState(db);
+  const [data] = useState(db);
   const [cart, setCart] = useState(intialCart);
 
   const MAX_ITEMS = 5;
